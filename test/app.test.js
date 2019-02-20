2c86a7de8deb7884f3614eb3526700cbc38cc8cf
owencyr <cyroc@dukes.jmu.edu>
2019-02-20 17:04:42 -0500

    playstore testing completed

--- test/app.test.js
+++ test/app.test.js
@@ -23,12 +23,32 @@ describe('playstore app', () => {
       });
   });
 
-  it('should return status 400 if sort is not title/rating', () => {
+  it('should return status 400 if sort is neither app nor rating', () => {
     return request(app)
       .get('/apps')
-      .query({ sort: numberOfCastMembers })
+      .query({ sort: 'numberOfCastMembers' })
       .expect(400, 'Sort must be one of title or rank');
   });
 
-  it('should accurately sort by title or ranking');
+  it('should accurately sort by app title', () => {
+    return (
+      request(app)
+        .get('/apps')
+        .query({ sort: 'app' })
+      // .expect(res).to.have.status(200)
+      // .expect(res).to.have.header('Content-Type')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .then(res => {
+          expect(res.body).to.be.an('array');
+          let i = 0;
+          let sorted = true;
+          while (sorted && i < res.body.length - 1) {
+            sorted = sorted && res.body[i].App < res.body[i + 1].App;
+            i++;
+          }
+          expect(sorted).to.be.true;
+        })
+    );
+  });
 });
